01ecd3f0b75b901412998beae9bf86a469d49a52
Jesper Stemann Andersen <jsa@ihp.dk>
2022-01-31 17:16:06 +0100

    Fixup explicit compat caret specifier

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -69,7 +69,7 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CUDNN_jll"; compat=string(CUDNN_version))
+    Dependency("CUDNN_jll"; compat="^$(CUDNN_version.major)")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.

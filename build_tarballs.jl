225e46cdd4f69ab8e53fd0b0254d1c7a2b7a66c9
Jesper Stemann Andersen <jsa@ihp.dk>
2022-01-31 15:11:30 +0100

    Fixup

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -70,7 +70,7 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CUDNN_jll"; compat=VersionNumber(CUDNN_version.major))
+    Dependency("CUDNN_jll"; compat=string(CUDNN_version.major))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
